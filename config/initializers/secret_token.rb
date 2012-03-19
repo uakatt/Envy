07c09a159474aaf6c9019c0376cc774eabe6e766
Sam Rawlins <sam.rawlins@gmail.com>
2012-03-19 08:58:43 -0700

    Just after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Envy::Application.config.secret_token = 'e8dfc7801fdbe25b6734af48ffd26dfdf921f655ae21c2bb3c723f5f48aade968917c3537b3d9eba923f9fbbc9d74caba9faa6b665fdd77830ca90cc5a3d51f0'
